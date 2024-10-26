21be57161cb4058a2f358c6ee1510a8a525f451c
Sergey <sergfox321@gmail.com>
2024-10-27 00:12:00 +0300

    Echo-dumb was fixed to process all arguments solving #2

--- src/echo-dumb.rs
+++ src/echo-dumb.rs
@@ -1,18 +1,17 @@
 use std::env::args;
 
 fn main() {
-    let args: Vec<String> = args().collect();
-    let echo_arg = &args[1];
-
-    let words: Vec<_> = echo_arg
-        .split(' ')
-        .collect();
-
-    let last_word = words[words.len()-1];
-
-    println!("{}", echo_arg);
-
-    for _ in 0..3 {
-        println!("{}", last_word);
+    for arg in args().skip(1) {
+        arg.split_whitespace().for_each(|word| { println!("{word}") });
+    }
+    match args().last() {
+        Some(last_arg) => if let Some(last_word) = last_arg
+            .split_whitespace()
+            .last() {
+                for _ in 0..3 {
+                    println!("{last_word}");
+                }
+        }
+        _ => (),
     }
 }
